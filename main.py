70f0e74982782782c37b613cce02b6ea5023118e
Daniil <155891153+MDS2009@users.noreply.github.com>
2025-02-06 16:09:26 +0300

    Обновление buttons и main

--- main.py
+++ main.py
@@ -1,186 +1,116 @@
 import pygame
-import os
 import sys
+from buttons import ImageButton
 
+# Инициализация Pygame
 pygame.init()
+
+# Полноэкранный режим
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 WIDTH, HEIGHT = screen.get_size()
-clock = pygame.time.Clock()
-FPS = 60
+pygame.display.set_caption("Главное меню")
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Шрифт
+font = pygame.font.Font(None, 36)
+
+# Громкость звука
+volume = 0.5
+pygame.mixer.init()
+pygame.mixer.music.set_volume(volume)
+
+# Создание кнопок
+play_button = ImageButton(WIDTH // 2 - 100, HEIGHT // 2 - 100, 200, 50, "", "play_b.png", "hover_play_b.png",
+                          "clic.mp3")
+exit_button = ImageButton(WIDTH // 2 - 100, HEIGHT // 2, 200, 50, "", "exite_b.png", "hover_exite_b.png",
+                          "clic.mp3")
+settings_button = ImageButton(WIDTH // 2 - 100, HEIGHT // 2 + 100, 200, 50, "", "settings.png",
+                              "hover_settings_b.png", "clic.mp3")
+
+# Фон главного меню
+menu_background = pygame.Surface((WIDTH, HEIGHT))
+menu_background.fill(BLACK)
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+def main_menu():
+    running = True
+    while running:
+        screen.fill(BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.USEREVENT and event.button == exit_button):
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.USEREVENT and event.button == play_button:
+                exec(open("basic_game.py").read())
+
+            if event.type == pygame.USEREVENT and event.button == settings_button:
+                settings_menu()
+
+            play_button.handle_event(event)
+            exit_button.handle_event(event)
+            settings_button.handle_event(event)
+
+        play_button.check_hover(pygame.mouse.get_pos())
+        play_button.draw(screen)
+        exit_button.check_hover(pygame.mouse.get_pos())
+        exit_button.draw(screen)
+        settings_button.check_hover(pygame.mouse.get_pos())
+        settings_button.draw(screen)
+        pygame.display.flip()
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
+def settings_menu():
+    global volume
+    increase_button = ImageButton(WIDTH // 2 + 50, HEIGHT // 2 - 50, 50, 50, "", "plus_b.png", "hover_plus_b.png",
+                                  "clic.mp3")
+    decrease_button = ImageButton(WIDTH // 2 - 100, HEIGHT // 2 - 50, 50, 50, "", "minus_b.png", "hover_minus_b.png",
+                                  "clic.mp3")
+    back_button = ImageButton(WIDTH // 2 - 100, HEIGHT // 2 + 100, 200, 50, "", "back_b.png", "hover_back_b.png",
+                              "clic.mp3")
 
+    run = True
+    while run:
+        screen.fill(BLACK)
 
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+        settings_text = font.render("Настройки", True, WHITE)
+        screen.blit(settings_text, (WIDTH // 2 - 80, HEIGHT // 4))
 
-    fon = pygame.transform.scale(load_image('main_fon.gif'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+        volume_text = font.render(f"Громкость: {int(volume * 100)}%", True, WHITE)
+        screen.blit(volume_text, (WIDTH // 2 - 80, HEIGHT // 2 - 50))
 
-    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.USEREVENT and event.button == increase_button:
+                if volume < 1.0:
+                    volume += 0.1
+                    pygame.mixer.music.set_volume(volume)
+
+            if event.type == pygame.USEREVENT and event.button == decrease_button:
+                if volume > 0.0:
+                    volume -= 0.1
+                    pygame.mixer.music.set_volume(volume)
+
+            if event.type == pygame.USEREVENT and event.button == back_button:
+                run = False
+
+            increase_button.handle_event(event)
+            decrease_button.handle_event(event)
+            back_button.handle_event(event)
+
+        increase_button.check_hover(pygame.mouse.get_pos())
+        increase_button.draw(screen)
+        decrease_button.check_hover(pygame.mouse.get_pos())
+        decrease_button.draw(screen)
+        back_button.check_hover(pygame.mouse.get_pos())
+        back_button.draw(screen)
+
         pygame.display.flip()
-        clock.tick(FPS)
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mar.png')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.pos = (pos_x, pos_y)
-
-    def move(self, x, y):
-        self.pos = (x, y)
-        self.rect = self.image.get_rect().move(
-            tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
-
-
-def move(hero, movement):
-    x, y = hero.pos
-    if movement == 'up':
-        if y > 0 and map_level[y - 1][x] == '.':
-            hero.move(x, y - 1)
-    if movement == 'down':
-        if y < level_y - 1 and map_level[y + 1][x] == '.':
-            hero.move(x, y + 1)
-    if movement == 'right':
-        if x < level_x - 1 and map_level[y][x + 1] == '.':
-            hero.move(x + 1, y)
-    if movement == 'left':
-        if x > 0 and map_level[y][x - 1] == '.':
-            hero.move(x - 1, y)
-
-
-'''class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)'''
-
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-start_screen()
-map_level = load_level('level.txt')
-player, level_x, level_y = generate_level(map_level)
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                move(player, 'up')
-            elif event.key == pygame.K_DOWN:
-                move(player, 'down')
-            elif event.key == pygame.K_LEFT:
-                move(player, 'left')
-            elif event.key == pygame.K_RIGHT:
-                move(player, 'right')
-
-    screen.fill((0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    clock.tick(FPS)
-    pygame.display.flip()
-pygame.quit()
